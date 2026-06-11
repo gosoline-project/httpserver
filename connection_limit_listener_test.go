21eb4e7fd6b84d3c677d943c874f57e7e4781276
Jan Kamieth <jan@justtrack.io>
2026-06-11 12:59:24 +0200

    feature: add rejected request metrics

--- connection_limit_listener_test.go
+++ connection_limit_listener_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/gosoline-project/httpserver"
 	httpserverMocks "github.com/gosoline-project/httpserver/mocks"
 	logMocks "github.com/justtrackio/gosoline/pkg/log/mocks"
-	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -17,30 +16,24 @@ type ConnectionLimitListenerTestSuite struct {
 	suite.Suite
 
 	listener      *httpserverMocks.NetListener
-	idleAvailable chan struct{}
 	manager       *httpserverMocks.ConnectionPressureManager
 	logger        logMocks.LoggerMock
+	idleAvailable chan struct{}
 }
 
-func TestConnectionLimitListenerTestSuite(t *testing.T) {
+func TestRunConnectionLimitListenerTestSuite(t *testing.T) {
 	suite.Run(t, new(ConnectionLimitListenerTestSuite))
 }
 
 func (s *ConnectionLimitListenerTestSuite) SetupTest() {
 	s.listener = httpserverMocks.NewNetListener(s.T())
-	s.idleAvailable = make(chan struct{})
 	s.manager = httpserverMocks.NewConnectionPressureManager(s.T())
 	s.logger = logMocks.NewLoggerMock(logMocks.WithMockAll, logMocks.WithTestingT(s.T()))
+	s.idleAvailable = make(chan struct{})
 }
 
 func (s *ConnectionLimitListenerTestSuite) TestDisabledReturnsOriginalListener() {
-	limited := httpserver.NewConnectionLimitListener(
-		s.T().Context(),
-		s.logger,
-		s.listener,
-		httpserver.ConcurrencySettings{},
-		s.manager,
-	)
+	limited := s.newLimited(0)
 
 	s.Same(s.listener, limited)
 }
@@ -49,33 +42,25 @@ func (s *ConnectionLimitListenerTestSuite) TestAcceptWaitsUntilConnectionSlotIsR
 	s.manager.EXPECT().CloseOldestIdleConnection().Return(false, nil).Maybe()
 	s.manager.EXPECT().IdleConnectionAvailable().Return(s.idleAvailable).Maybe()
 
-	limited := httpserver.NewConnectionLimitListener(
-		s.T().Context(),
-		s.logger,
-		s.listener,
-		httpserver.ConcurrencySettings{MaxConnections: 1},
-		s.manager,
-	)
+	limited := s.newLimited(1)
 
-	connA := httpserverMocks.NewNetConn(s.T())
-	connA.EXPECT().Close().Return(nil).Once()
-	s.listener.EXPECT().Accept().Return(connA, nil).Once()
+	s.mockAcceptedConn()
 	limitedConnA, err := limited.Accept()
-	s.Require().NoError(err)
-	s.Require().NotNil(limitedConnA)
+	s.NoError(err)
+	s.NotNil(limitedConnA)
 
 	acceptedSecond := atomic.Bool{}
 	s.listener.EXPECT().Accept().Return(httpserverMocks.NewNetConn(s.T()), nil).Once()
 	go func() {
 		connB, acceptErr := limited.Accept()
-		require.NoError(s.T(), acceptErr)
-		require.NotNil(s.T(), connB)
+		s.NoError(acceptErr)
+		s.NotNil(connB)
 		acceptedSecond.Store(true)
 	}()
 
 	s.Never(acceptedSecond.Load, 20*time.Millisecond, time.Millisecond)
 
-	s.Require().NoError(limitedConnA.Close())
+	s.NoError(limitedConnA.Close())
 	s.Eventually(acceptedSecond.Load, time.Second, time.Millisecond)
 }
 
@@ -87,34 +72,26 @@ func (s *ConnectionLimitListenerTestSuite) TestClosesIdleConnectionBeforeWaiting
 	s.manager.EXPECT().CloseOldestIdleConnection().Return(false, nil).Maybe()
 	s.manager.EXPECT().IdleConnectionAvailable().Return(s.idleAvailable).Maybe()
 
-	limited := httpserver.NewConnectionLimitListener(
-		s.T().Context(),
-		s.logger,
-		s.listener,
-		httpserver.ConcurrencySettings{MaxConnections: 1},
-		s.manager,
-	)
+	limited := s.newLimited(1)
 
-	connA := httpserverMocks.NewNetConn(s.T())
-	connA.EXPECT().Close().Return(nil).Once()
-	s.listener.EXPECT().Accept().Return(connA, nil).Once()
+	s.mockAcceptedConn()
 	limitedConnA, err := limited.Accept()
-	s.Require().NoError(err)
-	s.Require().NotNil(limitedConnA)
+	s.NoError(err)
+	s.NotNil(limitedConnA)
 
 	s.listener.EXPECT().Accept().Return(httpserverMocks.NewNetConn(s.T()), nil).Once()
 	acceptedSecond := atomic.Bool{}
 	go func() {
 		connB, acceptErr := limited.Accept()
-		require.NoError(s.T(), acceptErr)
-		require.NotNil(s.T(), connB)
+		s.NoError(acceptErr)
+		s.NotNil(connB)
 		acceptedSecond.Store(true)
 	}()
 
 	s.Eventually(closedIdle.Load, time.Second, time.Millisecond)
 	s.Never(acceptedSecond.Load, 20*time.Millisecond, time.Millisecond)
 
-	s.Require().NoError(limitedConnA.Close())
+	s.NoError(limitedConnA.Close())
 	s.Eventually(acceptedSecond.Load, time.Second, time.Millisecond)
 }
 
@@ -122,33 +99,25 @@ func (s *ConnectionLimitListenerTestSuite) TestWakesUpWhenIdleConnectionBecomesA
 	s.manager.EXPECT().CloseOldestIdleConnection().Return(false, nil).Maybe()
 	s.manager.EXPECT().IdleConnectionAvailable().Return(s.idleAvailable).Maybe()
 
-	limited := httpserver.NewConnectionLimitListener(
-		s.T().Context(),
-		s.logger,
-		s.listener,
-		httpserver.ConcurrencySettings{MaxConnections: 1},
-		s.manager,
-	)
+	limited := s.newLimited(1)
 
-	connA := httpserverMocks.NewNetConn(s.T())
-	connA.EXPECT().Close().Return(nil).Once()
-	s.listener.EXPECT().Accept().Return(connA, nil).Once()
+	s.mockAcceptedConn()
 	limitedConnA, err := limited.Accept()
-	s.Require().NoError(err)
-	s.Require().NotNil(limitedConnA)
+	s.NoError(err)
+	s.NotNil(limitedConnA)
 
 	s.listener.EXPECT().Accept().Return(httpserverMocks.NewNetConn(s.T()), nil).Once()
 	acceptedSecond := atomic.Bool{}
 	go func() {
 		connB, acceptErr := limited.Accept()
-		require.NoError(s.T(), acceptErr)
-		require.NotNil(s.T(), connB)
+		s.NoError(acceptErr)
+		s.NotNil(connB)
 		acceptedSecond.Store(true)
 	}()
 
 	s.Never(acceptedSecond.Load, 20*time.Millisecond, time.Millisecond)
 
-	s.Require().NoError(limitedConnA.Close())
+	s.NoError(limitedConnA.Close())
 	close(s.idleAvailable)
 	s.Eventually(acceptedSecond.Load, time.Second, time.Millisecond)
 }
@@ -158,18 +127,12 @@ func (s *ConnectionLimitListenerTestSuite) TestCloseUnblocksWaitingAccept() {
 	s.manager.EXPECT().IdleConnectionAvailable().Return(s.idleAvailable).Maybe()
 	s.listener.EXPECT().Close().Return(nil).Once()
 
-	limited := httpserver.NewConnectionLimitListener(
-		s.T().Context(),
-		s.logger,
-		s.listener,
-		httpserver.ConcurrencySettings{MaxConnections: 1},
-		s.manager,
-	)
+	limited := s.newLimited(1)
 
 	s.listener.EXPECT().Accept().Return(httpserverMocks.NewNetConn(s.T()), nil).Once()
 	connA, err := limited.Accept()
-	s.Require().NoError(err)
-	s.Require().NotNil(connA)
+	s.NoError(err)
+	s.NotNil(connA)
 
 	acceptedSecond := make(chan error, 1)
 	acceptReturned := atomic.Bool{}
@@ -181,7 +144,23 @@ func (s *ConnectionLimitListenerTestSuite) TestCloseUnblocksWaitingAccept() {
 
 	s.Never(acceptReturned.Load, 20*time.Millisecond, time.Millisecond)
 
-	s.Require().NoError(limited.Close())
-	s.Require().Eventually(acceptReturned.Load, time.Second, time.Millisecond)
+	s.NoError(limited.Close())
+	s.Eventually(acceptReturned.Load, time.Second, time.Millisecond)
 	s.ErrorIs(<-acceptedSecond, net.ErrClosed)
+}
+
+func (s *ConnectionLimitListenerTestSuite) newLimited(maxConnections int) net.Listener {
+	return httpserver.NewConnectionLimitListener(
+		s.T().Context(),
+		s.logger,
+		s.listener,
+		httpserver.ConcurrencySettings{MaxConnections: maxConnections},
+		s.manager,
+	)
+}
+
+func (s *ConnectionLimitListenerTestSuite) mockAcceptedConn() {
+	connA := httpserverMocks.NewNetConn(s.T())
+	connA.EXPECT().Close().Return(nil).Once()
+	s.listener.EXPECT().Accept().Return(connA, nil).Once()
 }
